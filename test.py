eea8ed2f2dc766d55f8a629e415c9216dff9f083
vladislav <ve@incsub.com>
2019-01-14 07:11:23 +0100

    Basic parser refactoring

--- test.py
+++ test.py
@@ -1,21 +1,7 @@
 import os
-import shutil
-import markdown
 
 from terp.routing import router
-
-def process_raw_input(root):
-    """Goes through item in source dir and generates initial data cache"""
-    md = markdown.Markdown(extensions=['meta'])
-    prepare_cache_directory(os.getcwd())
-    cache = []
-    for f in get_files(root):
-        data = process_raw_input_entry(f, md)
-        cache.append(data)
-        md.reset()
-
-    return cache
-
+from terp.sources import parser
 
 def generate_output(routed, out_dir):
     """Goes through the routed info and writes out the result using templates"""
@@ -52,52 +38,8 @@ def generate_item_output(item, out):
     print("Generating item output: {} using {}".format(path, item.get_template()))
     return True
 
-def process_raw_input_entry(path, md):
-    cwd = os.getcwd()
-    relpath = os.path.relpath(path, start=cwd)
-    destpath = os.path.join(get_cache_directory(cwd), relpath)
-    os.makedirs(os.path.dirname(destpath), exist_ok=True)
-    md.convertFile(
-        input=path,
-        output=destpath
-    )
-    data = {
-        "path": path,
-        "relpath": relpath,
-        "cached": destpath,
-    }
-    for (key, val) in md.Meta.items():
-        data[key] = val
-    return data
 
-
-def prepare_cache_directory(working_directory):
-    clear_cache_directory(working_directory)
-    working = get_cache_directory(working_directory)
-    os.makedirs(working)
-
-
-def clear_cache_directory(working_directory):
-    working = get_cache_directory(working_directory)
-    if os.path.exists(working):
-        print("directory exists, clearing")
-        shutil.rmtree(working)
-
-
-def get_cache_directory(working_directory):
-    return os.path.join(working_directory, '.terp-working', 'cache')
-
-
-def get_files(root):
-    """Gets list of files"""
-    return (
-        os.path.abspath(os.path.join(directory, filename))
-        for directory, subdir, files in os.walk(root)
-        for filename in files if os.path.splitext(filename)[1] == ".md"
-    )
-
-
-cache = process_raw_input('data')
+cache = parser.parse('data')
 routed = router.route(cache)
 report = generate_output(routed, 'out')
 
