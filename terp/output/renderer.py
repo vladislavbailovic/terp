fc4882d9498655595691f080624d519ee5ce804e
vladislav <ve@incsub.com>
2019-01-15 05:01:40 +0100

    Refactor rendering

---
+++ terp/output/renderer.py
@@ -0,0 +1,37 @@
+import os
+
+
+def out(routed, out_dir):
+    """Goes through the routed info and writes out the result using templates"""
+    cwd = os.getcwd()
+    results = []
+    for item in routed:
+        result = None
+        if hasattr(item, 'items'):
+            result = generate_taxonomy_output(item, out_dir)
+        else:
+            result = generate_item_output(item, out_dir)
+        results.append(result)
+
+    return results
+
+
+def generate_taxonomy_output(tax, out):
+    index = tax.get_index()
+    if index:
+        print("Generating index: {}".format(
+            index.get_type()
+        ))
+        generate_item_output(index, out)
+
+    for item in tax.items:
+        if hasattr(item, 'items'):
+            generate_taxonomy_output(item, out)
+
+    return True
+
+
+def generate_item_output(item, out):
+    path = os.path.join(out, item.get_destination())
+    print("Generating item output: {} using {}".format(path, item.get_template()))
+    return True
--- test.py
+++ test.py
@@ -1,43 +1,9 @@
-import os
-
 from terp.routing import router
 from terp.sources import parser
-
-def generate_output(routed, out_dir):
-    """Goes through the routed info and writes out the result using templates"""
-    cwd = os.getcwd()
-    results = []
-    for item in routed:
-        result = None
-        if hasattr(item, 'items'):
-            result = generate_taxonomy_output(item, out_dir)
-        else:
-            result = generate_item_output(item, out_dir)
-        results.append(result)
-
-    return results
-
-def generate_taxonomy_output(tax, out):
-    index = tax.get_index()
-    if index:
-        print("Generating index: {}".format(
-            index.get_type()
-        ))
-        generate_item_output(index, out)
-
-    for item in tax.items:
-        if hasattr(item, 'items'):
-            generate_taxonomy_output(item, out)
-
-    return True
-
-def generate_item_output(item, out):
-    path = os.path.join(out, item.get_destination())
-    print("Generating item output: {} using {}".format(path, item.get_template()))
-    return True
+from terp.output import renderer
 
 
 cache = parser.parse('data')
 routed = router.route(cache)
-report = generate_output(routed, 'out')
+report = renderer.out(routed, 'out')
 
